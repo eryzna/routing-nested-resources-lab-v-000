eaa6c26bd70eadff6b5b71c5a1df7c28696576e3
Elana Ryznar <eryzna@gmail.com>
2020-03-14 23:07:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
-    resources :songs, only: [:index, :show]
-  end
+  #resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+    #resources :songs, only: [:index, :show]
+  #end
 end
